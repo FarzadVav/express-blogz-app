eeb4caa9742670ceca035bf3b59f463f18ebf407
FarzadVav <farzad.vav.work@gmail.com>
2025-03-08 21:48:22 +0330

    add: all methods to posts.routes

--- src/routes/posts.routes.ts
+++ src/routes/posts.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import prisma from "../lib/db.js";
+import { authMiddleware } from "../middlewares/auth.middlewares.js";
 
 const postsRouter = Router()
 
@@ -35,6 +36,49 @@ postsRouter.get("/:id", async (req, res) => {
     }
 
     res.json(post)
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" })
+  }
+})
+
+postsRouter.post("/", authMiddleware, async (req, res) => {
+  const { title, content } = req.body
+  const user = req.user
+
+  try {
+    const post = await prisma.posts.create({
+      data: { title, content, authorId: user.id }
+    })
+
+    res.status(201).json(post)
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" })
+  }
+})
+
+postsRouter.put("/:id", authMiddleware, async (req, res) => {
+  const { id } = req.params
+  const { title, content } = req.body
+
+  try {
+    const post = await prisma.posts.update({
+      where: { id: +id },
+      data: { title, content }
+    })
+
+    res.json(post)
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" })
+  }
+})
+
+postsRouter.delete("/:id", authMiddleware, async (req, res) => {
+  const { id } = req.params
+
+  try {
+    await prisma.posts.delete({ where: { id: +id } })
+
+    res.json({ message: "Post deleted successfully" })
   } catch (error) {
     res.status(500).json({ message: "Internal server error" })
   }
